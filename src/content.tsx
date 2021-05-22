2e1ff39658757dafaf76a222663d216b4933238d
Merwan Achibet <achibet.merwan@gmail.com>
2021-05-22 22:00:35 +0200

    Display furagana, move tooltip next to selection

--- src/content.tsx
+++ src/content.tsx
@@ -1,64 +1,98 @@
 import { Box, Grid, Paper, Typography } from '@material-ui/core';
+import ScopedCssBaseline from '@material-ui/core/ScopedCssBaseline';
+import { createPopper } from '@popperjs/core';
+import { fit, FuriganaMatch } from 'furigana';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import root from 'react-shadow/material-ui';
+
 import { JishoApiEntry } from './jisho';
 
-console.log('Instant Jisho loaded');
-
 const container = document.createElement('div');
-container.id = 'instant-jisho';
+container.id = 'instant-jisho-container';
 document.body.appendChild(container);
 
+const target = document.createElement('div');
+target.style.background = 'rgba(255, 0, 0, 0.25)';
+target.style.position = 'absolute';
+document.body.appendChild(target);
+
+let done = false;
+
+//
+
 document.addEventListener('mouseup', async () => {
-  const selection = window.getSelection()?.toString();
+  const selection = window.getSelection();
 
   if (!selection) {
     return;
   }
 
-  chrome.runtime.sendMessage({ text: selection }, (response) => {
+  const selectedElement = selection.anchorNode?.parentElement;
+
+  if (!selectedElement) {
+    return;
+  }
+  console.log(selectedElement, selection.getRangeAt(0).getBoundingClientRect());
+
+  const bbox = selection.getRangeAt(0).getBoundingClientRect();
+
+  /*if (!done)*/ {
+    target.style.top = window.scrollY + bbox.top + 'px';
+    target.style.left = window.scrollX + bbox.left + 'px';
+    target.style.height = bbox.height + 'px';
+    target.style.width = bbox.width + 'px';
+    done = true;
+  }
+
+  chrome.runtime.sendMessage({ text: selection.toString() }, (response) => {
     console.log(response);
-    ReactDOM.render(<Main data={response.data.data} />, container);
+    ReactDOM.render(
+      // Put the UI container in a shadow root to prevent styles
+      // from host webpages interfering with our own styling rules
+      <root.div>
+        <ScopedCssBaseline>
+          <Main data={response.data.data} />
+        </ScopedCssBaseline>
+      </root.div>,
+      container,
+      () => createPopper(target, container)
+    );
   });
 });
 
-type Props = {
-  data: JishoApiEntry[];
-};
-
-function Main(props: Props) {
+function Main(props: { data: JishoApiEntry[] }) {
   const entry = props.data[0];
 
-  // We only list readings for the current slug
-  const readings = entry.japanese
-    .filter((j) => j.word && j.word === entry.slug)
-    .map((j) => j.reading);
+  // Generate furiganas
+
+  const readingsWritings = entry.japanese.filter(
+    (j) => j.word && j.reading && j.word === entry.slug
+  ); // TODO type not undef, rem !
+
+  const furiganas = readingsWritings
+    .map((rw) => fit(rw.word!, rw.reading!, { type: 'object', kanaReading: false }))
+    .filter((furi): furi is FuriganaMatch[] => furi !== null); // Filter out failures
+
+  console.log(furiganas);
 
   // Ignore "Wikipedia definition" entries that are often redundant
+
   const meanings = entry.senses.filter(
-    (m) => !m.parts_of_speech.map((p) => p.toLowerCase()).includes('wikipedia')
+    (m) => !m.parts_of_speech.some((p) => p.toLowerCase().includes('wikipedia'))
   );
 
   return (
     <Paper
-      style={{
-        position: 'fixed',
-        top: '50px',
-        right: '50px',
+      /*style={{
         zIndex: 2147483647
-      }}
+      }}*/
       elevation={4}
     >
       <Box m={1}>
         <Grid container direction='column' spacing={1}>
           <Grid item>
-            <Typography variant='h3'>{entry.slug}</Typography>
-          </Grid>
-
-          <Grid item>
-            {readings.map((r) => (
-              <Typography variant='h6'>{r}</Typography>
-            ))}
+            <Furigana readings={furiganas.length > 0 ? furiganas[0] : [{ w: entry.slug }]} />
           </Grid>
 
           {meanings.map((m, i) => (
@@ -68,7 +102,7 @@ function Main(props: Props) {
               ))*/}
 
               <Grid item>
-                <span style={{ color: 'grey' }}>{i}.</span>
+                <span style={{ color: 'grey' }}>{i + 1}.</span>
               </Grid>
 
               <Grid item>
@@ -84,5 +118,24 @@ function Main(props: Props) {
     </Paper>
   );
 }
+
+function Furigana(props: { readings: FuriganaMatch[] }) {
+  return (
+    <Grid container direction='row' wrap='nowrap'>
+      {props.readings.map((match) => (
+        <Grid item container direction='column' alignItems='center'>
+          <Grid item>
+            <Typography variant='h3'>{match.w}</Typography>
+          </Grid>
+          <Grid item>
+            <Typography variant='subtitle1'>{match.r}</Typography>
+          </Grid>
+        </Grid>
+      ))}
+    </Grid>
+  );
+}
+
+console.log('Instant Jisho loaded');
 
 export {};
